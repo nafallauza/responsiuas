6094ab79d197e3b1e73abe038b50f1f612a85d68
nafal lauza hafidh adani <nafallauza@gmail.com>
2025-07-13 21:47:02 +0700

    alo

--- frontend/src/components/Education.vue
+++ frontend/src/components/Education.vue
@@ -4,7 +4,7 @@ import axios from 'axios';
 import SectionTitle from './SectionTitle.vue';
 const educationHistory = ref([]);
 onMounted(async () => {
-try { const response = await axios.get('http://localhost:3000/api/education'); educationHistory.value
+try { const response = await axios.get('/api/education'); educationHistory.value
 = response.data; } catch (error) { console.error(error); }
 });
 </script>
--- frontend/src/components/Skills.vue
+++ frontend/src/components/Skills.vue
@@ -7,7 +7,7 @@ const skills = ref([])
 
 onMounted(async () => {
   try {
-    const response = await axios.get('http://localhost:3000/api/skills')
+    const response = await axios.get('/api/skills')
     skills.value = response.data
   } catch (error) {
     console.error(error)
